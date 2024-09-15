75100fcf153541af582f40afbacda97964c08e47
WooGi1020 <jungwk1020@gmail.com>
2024-09-16 01:42:35 +0900

    feat: about 관련 컴포넌트 default export

---
+++ src/app/about/_components/index.ts
@@ -0,0 +1,5 @@
+export { default as AboutMeta } from "./AboutMeta";
+export { default as AboutOnline } from "./AboutOnline";
+export { default as AboutOpinion } from "./AboutOpinion";
+export { default as AboutProject } from "./AboutProject";
+export { default as AboutHistory } from "./AboutHistory";
