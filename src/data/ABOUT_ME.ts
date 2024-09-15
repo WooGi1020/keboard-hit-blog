5349136b556404df27a88f86bb5c5e96a11479b3
WooGi1020 <jungwk1020@gmail.com>
2024-09-16 00:51:45 +0900

    feat: about 페이지에 사용할 객체 정의

---
+++ src/data/ABOUT_ME.ts
@@ -0,0 +1,42 @@
+const ABOUT_ME = {
+  name: "정우기",
+  avatar: "https://avatars.githubusercontent.com/u/113000290?v=4",
+  role: "컨셉에 충실한 프론트엔드 엔지니어",
+  address: "대한민국 경기도",
+  description:
+    "프로젝트는 정해진 컨셉을 유지하고 관리하는 일이며, 이를 이끄는 개발자야말로 항상 컨셉에 충실해야한다고 생각합니다.\n하고자 하는 일을 위해 노력하고 주어진 환경에 잘 적응한다는 저만의 컨셉을 유지하기 위해 노력합니다.",
+  online: [
+    {
+      name: "Github",
+      link: "https://github.com/WooGi1020",
+    },
+  ],
+  project: [
+    {
+      name: "Message-Bloom",
+      description: "봄날의 선물같은 메세지를 공유하는 웹서비스",
+      link: "https://github.com/WooGi1020/MessageBloom",
+      period: "2024-04-30 ~ 2024-05-17",
+      tag: ["React", "SCSS"],
+      src: "/images/messageBloom.png",
+    },
+    {
+      name: "Wiki-Viki",
+      description: "서로의 위키를 작성하고 공유하는 웹서비스",
+      link: "https://github.com/WooGi1020/wiki-viki",
+      period: "2024-06-21 ~ 2024-07-07",
+      tag: ["Next.js", "Tailwind", "Typescript"],
+      src: "/images/wikiViki.png",
+    },
+    {
+      name: "Coworkers",
+      description: "팀원들과 투두리스트를 공유하며 관리하는 웹서비스",
+      link: "https://github.com/WooGi1020/CCC-coworkers",
+      period: "2024-07-29 ~ 2024-08-28",
+      tag: ["Next.js", "Tailwind", "Typescript", "Shadcn", "Pusher"],
+      src: "/images/coworkers.png",
+    },
+  ],
+};
+
+export default ABOUT_ME;
