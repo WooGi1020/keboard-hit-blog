1e9ee20ccdd3755d168e7f64f40bdb9697b5becf
WooGi1020 <jungwk1020@gmail.com>
2024-09-16 23:54:44 +0900

    refactor: 포스트 페이지로 홈페이지 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-import dynamic from "next/dynamic";
-
-const AnimationPlayer = dynamic(() => import("@/components/animation/lottieKeyboard"), {
-  ssr: false,
-});
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <section className="max-w-[1000px] mx-auto flex flex-col gap-10">
-      <AnimationPlayer className="lottie-animation mx-auto relative bottom-8" />
-    </section>
-  );
+  return redirect("/posts");
 }
