fef361335383d323f979aab5cbdb9d6012800af0
WooGi1020 <jungwk1020@gmail.com>
2024-09-16 23:52:29 +0900

    remove: 불필요 파일 제거

--- src/app/posts/Posts.tsx
+++
@@ -1,9 +0,0 @@
-function Posts() {
-  return (
-    <div>
-      <h2>최근 게시글</h2>
-    </div>
-  );
-}
-
-export default Posts;
--- src/app/posts/[slug]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function page({ params }: { params: { slug: any } }) {
-  return <div>{params.slug}</div>;
-}
-
-export default page;
