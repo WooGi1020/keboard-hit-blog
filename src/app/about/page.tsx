0e3dd4cb2d33c879e8613d4561c78318b2a6fa6c
WooGi1020 <jungwk1020@gmail.com>
2024-09-20 11:43:17 +0900

    design: 애니메이션 추가

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ import {
 
 function AboutPage() {
   return (
-    <div className="flex flex-col gap-11 w-full">
+    <div className="flex flex-col gap-11 w-full post">
       <h2 className="text-center text-3xl font-bold text-animation mb-[-20px]">반갑습니다! 😊</h2>
       <AboutMeta />
       <AboutOpinion />
