1f7175cd27169c91748c7d8784169e8b5fc182b6
WooGi1020 <jungwk1020@gmail.com>
2024-09-20 11:43:37 +0900

    featL readingTime 함수 사용

--- src/app/posts/_components/PostCard.tsx
+++ src/app/posts/_components/PostCard.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import React from "react";
 import { Calendar, Clock } from "lucide-react";
 import dayjs from "dayjs";
+import readingTimeFunc from "@/lib/readingTime";
 
 interface Post {
   post: { slug: string };
@@ -12,15 +13,16 @@ interface Post {
 async function PostCard({ post }: Post) {
   const data = getPostMeta(post);
   const formattedDate = dayjs(data.frontmatter.date);
+  const readTime = readingTimeFunc(data.content);
 
   return (
     <Link
       href={`/posts/${post.slug}`}
-      className="card hover:hover-card border-2 border-input w-[450px] h-[450px] rounded-xl"
+      className="card hover:hover-card border-2 border-input w-[450px] h-[450px] rounded-xl post"
     >
       <div className="w-full h-[250px] relative">
         <Image
-          src={`/images/thumnails/${post.slug}/test.jpg`}
+          src={`/images/thumbnails/${post.slug}.jpg`}
           alt="포스트 썸네일 이미지"
           fill
           priority
@@ -33,11 +35,12 @@ async function PostCard({ post }: Post) {
         <p className="text-sm">{data.frontmatter.description}</p>
         <div className="mt-auto flex justify-between">
           <div className="flex gap-2">
-            <Calendar className="size-5" />
+            <Calendar className="size-5 items-center" />
             <span>{formattedDate?.format("YYYY년 MM월 DD일")}</span>
           </div>
           <div className="flex gap-2">
-            <Clock className="size-5" />
+            <Clock className="size-5 items-center" />
+            <span>{readTime}분</span>
           </div>
         </div>
       </div>
