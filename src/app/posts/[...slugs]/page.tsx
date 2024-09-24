753a0f90f84fb77fecd881ffe4c18bc40d8b1c35
WooGi1020 <jungwk1020@gmail.com>
2024-09-24 10:41:28 +0900

    remove: Image 컴포넌트 위치 변경으로 인한 제거

--- src/app/posts/[...slugs]/page.tsx
+++ src/app/posts/[...slugs]/page.tsx
@@ -2,7 +2,6 @@ import { getAllPosts, parsePosts } from "@/lib/getPosts";
 import dynamic from "next/dynamic";
 import type { Params } from "@blogType";
 import PostMeta from "@/app/posts/[...slugs]/_components/PostMeta";
-import Image from "next/image";
 import PostContent from "@/app/posts/[...slugs]/_components/PostContent";
 import { notFound } from "next/navigation";
 
@@ -29,15 +28,7 @@ async function postPage({ params }: { params: Params }) {
       </div>
       <PostMeta params={params} />
       <div className="border dark:border-gray-500 border-gray-400 mt-[-20px]" />
-      <div className="relative w-full h-[400px] mb-[20px]">
-        <Image
-          src={`/images/thumbnails/${imagePath}.jpg`}
-          alt="포스트 썸네일 이미지"
-          fill
-          className="object-cover"
-        />
-      </div>
-      <PostContent content={postData.content} />
+      <PostContent content={postData.content} imagePath={imagePath} />
     </section>
   );
 }
