eed8f1682a7d408a94b48ca4fd43539152582ce2
WooGi1020 <jungwk1020@gmail.com>
2024-09-26 11:57:48 +0900

    design: footer 디자인 조정

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,9 +5,9 @@ function Footer() {
     <footer className="w-full h-[150px] p-5 flex flex-col justify-center">
       <div
         className="max-w-[982px] px-[16px] mx-auto w-full
-      flex flex-col gap-2"
+      flex flex-col gap-2 items-center"
       >
-        <p>
+        <p className="text-lg">
           코딩기의 <span className="text-chart-2">Next.js</span> 블로그: 키보드 두들기며 먹고살기
           ver.1
         </p>
