1822fac28cc9e6963cc2d06dfc098ba0ce6b7ccc
WooGi1020 <jungwk1020@gmail.com>
2024-09-27 17:10:04 +0900

    feat: 경로에 따른 로직 수정

--- src/app/posts/_components/Posts.tsx
+++ src/app/posts/_components/Posts.tsx
@@ -1,8 +1,18 @@
 import PostCard from "@/app/posts/_components/PostCard";
-import { getAllPosts } from "@/lib/getPosts";
+import { getPostsByTag } from "@/lib/getPosts";
+import { notFound } from "next/navigation";
 
-async function Posts() {
-  const posts = await getAllPosts();
+async function Posts({ tag }: { tag?: string }) {
+  let posts: { slug: string }[];
+  if (!tag) {
+    posts = await getPostsByTag();
+  } else {
+    posts = await getPostsByTag(tag!);
+  }
+
+  if (posts.length === 0) {
+    notFound();
+  }
 
   return (
     <div className="flex flex-col items-center justify-center max-w-[950px] w-full mx-auto">
