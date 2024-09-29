be48f89d85c1e86edd371d172caa3553ed9ac038
WooGi1020 <jungwk1020@gmail.com>
2024-09-29 23:42:41 +0900

    feat: META_DATA 상수객체 정의

--- src/constant/META_DATA.ts
+++ src/constant/META_DATA.ts
@@ -1,3 +1,11 @@
-const META_DATA = {};
+const META_DATA = {
+  title: "키보드 두들기며 먹고살기",
+  sitename: "코딩기의 Next.js 블로그 | 키보드 두들기며 먹고살기",
+  description:
+    "Next.js와 tailwind로 만들고 mdx 파일 파싱을 활용한 포스트를 업로드하는 블로그입니다.",
+  keyword: ["Next.js", "블로그 프로젝트", "tailwind", "토이 프로젝트"],
+  url: "https://keyboard-hit-blog.vercel.app/",
+  ogImage: "/images/opgraph/op-image.png",
+};
 
 export default META_DATA;
