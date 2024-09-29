177155082f74d935916d558ca38c55c9a53f369c
WooGi1020 <jungwk1020@gmail.com>
2024-09-29 23:45:04 +0900

    feat: 사이트맵 함수 추가

---
+++ src/app/sitemap.ts
@@ -0,0 +1,35 @@
+import { getAllPostUrl } from "@/lib/getPosts";
+import type { MetadataRoute } from "next";
+
+const defaultSiteMaps: MetadataRoute.Sitemap = [
+  {
+    url: "https://keyboard-hit-blog",
+    lastModified: new Date(),
+    changeFrequency: "daily",
+    priority: 1,
+  },
+  {
+    url: "https://keyboard-hit-blog/about",
+    lastModified: new Date(),
+    changeFrequency: "daily",
+    priority: 0.8,
+  },
+  {
+    url: "https://keyboard-hit-blog/posts/all",
+    lastModified: new Date(),
+    changeFrequency: "daily",
+    priority: 0.8,
+  },
+];
+
+const postMetadata = getAllPostUrl();
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const sitemapFromPosts: MetadataRoute.Sitemap = postMetadata.map((post) => ({
+    url: `https://keyboard-hit-blog/posts/${post.path}`,
+    lastModified: new Date(post.date),
+    changeFrequency: "daily",
+    priority: 0.7,
+  }));
+  return [...defaultSiteMaps, ...sitemapFromPosts];
+}
