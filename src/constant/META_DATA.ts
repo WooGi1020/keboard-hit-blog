5b5889654aff7d45b57fe8e7f098b9c011c31207
WooGi1020 <jungwk1020@gmail.com>
2024-10-01 19:28:35 +0900

    refactor: 메타데이터 구글 서치콘솔 키값 변경

--- src/constant/META_DATA.ts
+++ src/constant/META_DATA.ts
@@ -4,7 +4,7 @@ const META_DATA = {
   description:
     "Next.js와 tailwind로 만들고 mdx 파일 파싱을 활용한 포스트를 업로드하는 블로그입니다.",
   keyword: ["Next.js", "블로그 프로젝트", "tailwind", "토이 프로젝트"],
-  googleVerification: "Gq3mCEzs90XipoPxYsJNeXI2_e06fF4tkP4Gs03ZlRk",
+  googleVerification: "AgIk2QRivYxvtfsP9j5WW2maKM50mBcrzk8azyn9HeM",
   url: "https://keyboard-hit-blog.vercel.app/",
   ogImage: "/images/opgraph/op-image.png",
 };
